3aad789343a4a1107cdb3f9c7f0dea02dc1986fd
yeahyung <yhb77888@naver.com>
2023-08-29 23:50:59 +0900

    Lock 주석 추가

--- src/main/java/com/example/stock_example/service/StockService.java
+++ src/main/java/com/example/stock_example/service/StockService.java
@@ -26,6 +26,19 @@ public class StockService {
     synchronized: 하나의 thread 만 메서드에 접근할 수 있도록 메서드 선언부에 설정할 수 있음
     하지만 synchronized 는 하나의 프로세스에서만 정상 동작함을 보장함
     즉, 서버가 1대가 아니라면 재소감소가 정상적으로 이루어졌다고 보장할 수 없음, 따라서 DB 에서의 Lock 을 활용해야함
+
+    1. Pessimistic Lock
+        - 실제로 데이터에(table or row level) lock 을 걸어서 정합성을 맞추는 방법
+        - exclusive lock 을 걸게되면 다른 트랜잭션에서는 lock 이 해제되기 전까지 데이털르 가져갈 수 없게 됨
+        - 데드락 발생 가능성
+    2. Optimistic Lock
+        - 실제 Lock 을 이요하지 않고 버전을 이용하여 정합성을 맞춤
+        - 데이터를 읽은 후 update 를 수행할 때 내가 읽은 버전이 맞는지 확인하여 업데이트
+            - 내가 읽은 버전과 맞지 않는다면 application 에서 다시 읽은 후에 작업 수행
+    3. Named Lock
+        - metadata lock
+        - lock 획득 후 해제하기 전까지 다른 세션에서는 해당 lock 을 획득할 수 없음
+        - transaction 이 종료될 때 lock 이 자동으로 해제되지 않음, 별도 명령어로 해제를 수행해주거나 선점시간이 끝나야 ㅎ제됨
      */
     public synchronized void decrease(Long id, Long quantity) {
         // Stock 조회
